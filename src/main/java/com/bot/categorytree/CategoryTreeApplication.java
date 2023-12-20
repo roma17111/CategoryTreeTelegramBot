b0129e57b133cd5bcee59408e82ac197fb5e8419
roman <romanze1706@gmail.com>
2023-12-20 13:00:51 +0300

    Создан проект

---
+++ src/main/java/com/bot/categorytree/CategoryTreeApplication.java
@@ -0,0 +1,13 @@
+package com.bot.categorytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryTreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bot/categorytree/CategoryTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.categorytree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryTreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
